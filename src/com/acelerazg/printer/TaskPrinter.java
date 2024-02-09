11d241155e933601829178083d85fe803958c68e
Artur Pedro <arturpedromartins@gmail.com>
2024-02-09 19:55:25 -0300

    Sorting and Printing methods

--- src/com/acelerazg/printer/TaskPrinter.java
+++ src/com/acelerazg/printer/TaskPrinter.java
@@ -1,17 +1,38 @@
 package com.acelerazg.printer;
 
 import com.acelerazg.app.Task;
-
+import com.acelerazg.comparators.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TaskPrinter {
     public static void allByPriority(ArrayList<Task> tasks) {
-
+        Collections.sort(tasks, new PriorityComparator());
+        for (Task t : tasks) {
+            System.out.println(t);
+        }
+    }
+    public static void allByStatus(ArrayList<Task> tasks) {
+        Collections.sort(tasks, new StatusComparator());
+        for (Task t : tasks) {
+            System.out.println(t);
+        }
+    }
+    public static void allByTag(ArrayList<Task> tasks) {
+        Collections.sort(tasks, new TagComparator());
+        for (Task t : tasks) {
+            System.out.println(t);
+        }
     }
     public static void askCommand() {
-
+        System.out.print("\nWaiting for command to execute: "+
+                TextColors.ANSI_GREEN_BACKGROUND + TextColors.ANSI_BLACK + "[New]" + TextColors.ANSI_RESET + " " +
+                TextColors.ANSI_RED_BACKGROUND + TextColors.ANSI_BLACK + "[Delete]" + TextColors.ANSI_RESET + " " +
+                TextColors.ANSI_BLUE_BACKGROUND + TextColors.ANSI_BLACK + "[Update]" + TextColors.ANSI_RESET + " " +
+                TextColors.ANSI_YELLOW_BACKGROUND + TextColors.ANSI_BLACK + "[View]" + TextColors.ANSI_RESET + " " +
+                "\n> ");
     }
     public static void closeApp() {
-
+        System.out.println("Saving list and closing application...");
     }
 }
