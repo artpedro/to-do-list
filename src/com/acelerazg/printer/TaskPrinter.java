93977f4456a4082e27b08bb68a0f7d09dcc1ef62
Artur Pedro <arturpedromartins@gmail.com>
2024-02-09 17:30:23 -0300

    Methods declared

--- src/com/acelerazg/printer/TaskPrinter.java
+++ src/com/acelerazg/printer/TaskPrinter.java
@@ -1,4 +1,17 @@
 package com.acelerazg.printer;
 
+import com.acelerazg.app.Task;
+
+import java.util.ArrayList;
+
 public class TaskPrinter {
+    public static void allByPriority(ArrayList<Task> tasks) {
+
+    }
+    public static void askCommand() {
+
+    }
+    public static void closeApp() {
+
+    }
 }
