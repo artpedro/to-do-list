a7cfa092f8aa7c40f73db4933f212c89bee0f6c1
Artur Pedro <arturpedromartins@gmail.com>
2024-02-09 23:41:11 -0300

    Main functional

--- src/com/acelerazg/app/Application.java
+++ src/com/acelerazg/app/Application.java
@@ -1,7 +1,7 @@
 package com.acelerazg.app;
-// import com.acelerazg.printer.TaskPrinter;
-// import com.acelerazg.filemanager.TaskFileReader;
 import com.acelerazg.printer.TaskPrinter;
+import com.acelerazg.filemanager.TaskFileReader;
+import java.util.ArrayList;
 
 import static com.acelerazg.app.Utils.getInput;
 
@@ -9,29 +9,33 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("TO-DO LIST");
         System.out.println();
-        // TaskFileReader fileReader = new TaskFileReader(path);
-        // if (fileReader.hasLog()) { // check if file is readable
-        //      ArrayList<Task> allTasks = fileReader.getLog(); // create one instance of Task for each line in log
-        //      TaskPrinter.allByPriority(allTasks); //
-        // } else {System.out.println("No records found!);
-
+        TaskFileReader fileReader = new TaskFileReader();
+        ArrayList<Task> allTasks = fileReader.readLog(); // create one instance of Task for each line in log
+        TaskPrinter.allByPriority(allTasks);
         System.out.println();
         while (true) {
-            // TaskPrinter.askCommand();
+            TaskPrinter.askCommand();
             String command = getInput().toLowerCase();
             if (command.equals("close")) break;
-//            switch (command) {
-//                case "new": TaskHandler.newTask(allTasks);
-//                    break;
-//                case "delete": TaskHandler.deleteTask(allTasks);
-//                    break;
-//                case "update": TaskHandler.updateTask(allTasks);
-//                    break;
-//                case "view": TaskHandler.viewTask(allTask);
-//                    break;
-//                default: System.out.println("Invalid Command");
-//            }
+          switch (command) {
+                case "new":
+                    TaskHandler.newTask(allTasks);
+                    fileReader.saveLog(allTasks);
+                    break;
+                case "delete":
+                    TaskHandler.deleteTask(allTasks);
+                    fileReader.saveLog(allTasks);
+                    break;
+                case "update":
+                    TaskHandler.updateTask(allTasks);
+                    fileReader.saveLog(allTasks);
+                    break;
+                case "view":
+                    TaskHandler.viewTask(allTasks);
+                    break;
+                default: System.out.println("Invalid Command");
+            }
         }
-        // TaskPrinter.closeApp();
+        TaskPrinter.closeApp();
     }
 }
